f06db6266c12941279ed3a6cfefda92467a24da3
Luybov Kurij <lubovhimicka@gmail.com>
2023-05-01 15:33:53 +0300

    feat: add file style.css

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //build HTML structure
-let main = document.createElement ('main');
-main.classList.add('main');
-document.body.appendChild(main);
 
-const textarea = document.createElement('textarea');
+let textarea = document.createElement('textarea');
 textarea.classList.add('input-field');
 document.body.appendChild(textarea);
 
-const keybordEnglish = [ 
+
+ let conteiner = document.createElement("div");
+ conteiner.classList.add("keybord-conteiner");
+ 
+ let keybordEnglish = [ 
     "~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
     "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/",
     "caps lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter",
@@ -17,28 +18,32 @@ const keybordEnglish = [
 
 for (let key of keybordEnglish) {
 
-  const buttons = document.createElement("button");
+  let buttons = document.createElement("button");
   buttons.classList.add("button-key");
   buttons.innerText = key;
   document.body.appendChild(buttons);
 
+ conteiner.append(buttons);
 
-const conteiner = document.createElement('div');
-conteiner.classList.add('keybord-conteiner');
-conteiner.append(buttons);
+} 
+
 document.body.appendChild(conteiner);
-}
 
 
-const text = document.createElement('p');
+
+let text = document.createElement('p');
 text.classList.add('text');
 text.innerText = "Windows OS keyboard";
-document.body.appendChild(text);
 
-const switchLang = document.createElement("p");
+
+let switchLang = document.createElement("p");
 switchLang.classList.add("text");
-switchLang.innerText = "Switch language keyboard shortcut SHIFT+ALT";
-document.body.appendChild(switchLang);
+switchLang.innerText = "Switch language keyboard shortcut Shift+Alt";
+
+document.body.append(text, switchLang);
+
+
+
 
 const keybordUkraine = [
     "~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
